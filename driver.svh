bd21aa0a5ac553bf0b5eb42f6dd4fb9f9e384fa8
SG295 <sethg295@gmail.com>
2024-12-05 15:58:09 -0500

    update transaction file

--- driver.svh
+++ driver.svh
@@ -26,7 +26,7 @@ class driver extends uvm_driver#(transaction);
     forever begin
       seq_item_port.get_next_item(req_item);
       DUT_reset();
-      vif.signal = req_item;
+      vif.signal = req_item.signal;
       #(0.2)
       @(posedge vif.clk);
       seq_item_port.item_done();
--- transaction.svh
+++ transaction.svh
@@ -4,19 +4,14 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class transaction #(parameter NUM_BITS = 4) extends uvm_sequence_item;
-  rand bit [NUM_BITS - 1:0] a;
-  rand bit [NUM_BITS - 1:0] b;
-  rand bit carry_in;
-  bit [NUM_BITS - 1:0] result_sum;
-  bit result_overflow;
+class transaction #(parameter NUM_BITS = 1) extends uvm_sequence_item;
+  rand bit [NUM_BITS - 1:0] signal;
+  bit [NUM_BITS - 1:0] pos_edge, neg_edge;
 
   `uvm_object_utils_begin(transaction)
-    `uvm_field_int(a, UVM_NOCOMPARE)
-    `uvm_field_int(b, UVM_NOCOMPARE)
-    `uvm_field_int(carry_in, UVM_NOCOMPARE)
-    `uvm_field_int(result_sum, UVM_DEFAULT)
-    `uvm_field_int(result_overflow, UVM_DEFAULT)
+    `uvm_field_int(signal, UVM_NOCOMPARE)
+    `uvm_field_int(pos_edge, UVM_DEFAULT)
+    `uvm_field_int(neg_edge, UVM_DEFAULT)
   `uvm_object_utils_end
 
   // add constrains for randomization
@@ -28,7 +23,7 @@ class transaction #(parameter NUM_BITS = 4) extends uvm_sequence_item;
   // if two transactions are the same, return 1
   function int input_equal(transaction tx);
     int result;
-    if((a == tx.a) && (b == tx.b) && (carry_in == tx.carry_in)) begin
+    if((signal == tx.signal)) begin
       result = 1;
       return result;
     end
