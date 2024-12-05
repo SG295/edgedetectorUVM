39abe7ae79c1fc0af6a1381f6bc9f29278d15c3e
SG295 <sethg295@gmail.com>
2024-12-05 15:59:13 -0500

    whoops needed check in IF

--- edgedetector_if.svh
+++ edgedetector_if.svh
@@ -7,7 +7,7 @@ interface edgedetector_if #(parameter WIDTH = 1) (input logic clk);
   logic [WIDTH - 1:0] signal;
   logic [WIDTH - 1:0] pos_edge, neg_edge;
   
-  // logic check = 1;
+  logic check = 1;
 
   modport tester
   (
